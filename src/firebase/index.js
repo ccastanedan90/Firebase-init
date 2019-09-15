495bcfdab5812501718be59c3dd97d7ea7273bc2
carloscn90 <carloscastanedan@gmail.com>
2019-09-15 09:51:22 -0500

    added: firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD--IqjQda1iQx3fnk5UhrgoXpNI7MFTsU",
+    authDomain: "firabase-init-4f956.firebaseapp.com",
+    databaseURL: "https://firabase-init-4f956.firebaseio.com",
+    projectId: "firabase-init-4f956",
+    storageBucket: "",
+    messagingSenderId: "169536461590",
+    appId: "1:169536461590:web:01822991242965718c8e37"
+  }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import Modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase'
 
 // Import Components
 import App from './components/App/App';
@@ -8,8 +9,12 @@ import App from './components/App/App';
 // Import Styles
 import './styles/index.css';
 
+// Import Utils
+import firebaseConfig from './firebase'
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp(firebaseConfig)
+serviceWorker.unregister();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
